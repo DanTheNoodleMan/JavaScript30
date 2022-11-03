99e8683465971a1bb8bdab2cac64f70eb1490321
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-03 12:15:12 +0100

    Plays sound corresponding to key pressed and adds animation to key pressed but doesn't revert the animation when not pressed, to do

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,5 +1,10 @@
 window.addEventListener('keydown', function(e) {
-    console.log(e);
-    const audio = document.querySelector(`audio[data-key=${e.code}]`)
+    const audio = document.querySelector(`audio[data-key=${e.key}]`);
+    const key =  document.querySelector(`.key[data-key=${e.key}]`);
+    console.log(key);
+    if(!audio) return;
+    audio.currentTime = 0; //allows for sounds to be spammed
+    audio.play();
+    key.classList.add('playing');
 });
 
